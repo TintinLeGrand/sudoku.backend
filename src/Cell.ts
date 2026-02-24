c3a4ff63fd46a2d96b6e6c218b5dcd5f0ab08073
TintinLeGrand <ethan.leneindre9@gmail.com>
2026-02-25 00:57:47 +0100

    feat: Implement Cell features

--- src/Cell.ts
+++ src/Cell.ts
@@ -27,15 +27,50 @@ export class Cell {
 	}
 
 	public put(number: number, category: CellCategory): boolean {
+		if (isIllegalNumber(number)) return false;
+		if (this._final) return false;
+
+		if (category === CellCategory.Normal) {
+			this._number = number;
+			return true;
+		} else if (category === CellCategory.Draft) {
+			if (this._drafts.includes(number)) return false;
+			this._drafts.push(number);
+			return true;
+		}
 		return false;
 	}
 
 	public erase(number: number, category: CellCategory): boolean {
+		if (isIllegalNumber(number)) return false;
+		if (this._final) return false;
+
+		if (category === CellCategory.Normal) {
+			if (this._number === number) {
+				this._number = null;
+				return true;
+			}
+			return false;
+		} else if (category === CellCategory.Draft) {
+			const index = this._drafts.indexOf(number);
+			if (index !== -1) {
+				this._drafts.splice(index, 1);
+				return true;
+			}
+			return false;
+		}
 		return false;
 	}
 
 	public finalize(): boolean {
-		this._final = true;
+		if (this._number !== null && !this._final) {
+			this._final = true;
+			return true;
+		}
 		return false;
 	}
+}
+
+function isIllegalNumber(number: number) {
+	return number < 1 || number > 9 || !Number.isInteger(number);
 }
