785e93f693bebcd992e41fc7dd5c111dbe702fb7
TintinLeGrand <ethan.leneindre9@gmail.com>
2026-02-25 00:04:40 +0100

    test: Add test cases and Cell skeleton

--- src/Cell.ts
+++ src/Cell.ts
@@ -1,1 +1,43 @@
-export class Cell {}
+export enum CellType {
+	Empty = "empty",
+	Final = "final",
+	Draft = "draft",
+	Written = "written",
+}
+
+export class Cell {
+	private _number: number | null;
+	private _drafts: number[];
+	private _type: CellType;
+
+	constructor() {
+		this._number = null;
+		this._drafts = [];
+		this._type = CellType.Empty;
+	}
+
+	get number(): number | null {
+		return this._number;
+	}
+
+	get drafts(): number[] {
+		return this._drafts;
+	}
+
+	get type(): CellType {
+		return this._type;
+	}
+
+	public put(number: number, as: CellType.Draft | CellType.Written): boolean {
+		return false;
+	}
+
+	public erase(): boolean {
+		return false;
+	}
+
+	public finalize(): boolean {
+		this._type = CellType.Final;
+		return false;
+	}
+}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,73 @@
-import { describe, expect, test } from "bun:test";
+import { describe, expect, it, test } from "bun:test";
+import { Cell } from "../src";
+import { CellType } from "../src/Cell";
 
 test("default tests", () => {
 	expect(1 + 1).toBe(2);
+});
+
+describe("cell tests", () => {
+	it("Should create an empty cell", () => {
+		const cell = new Cell();
+		expect(cell.number).toBeNull();
+		expect(cell.drafts).toBeArrayOfSize(0);
+		expect(cell.type).toBe(CellType.Empty);
+	});
+
+	it("Should set to 1 an empty cell", () => {
+		const cell = new Cell();
+		const number = 1;
+		expect(cell.number).toBeNull();
+		expect(cell.put(number, CellType.Written)).toBeTrue();
+		expect(cell.number).toBe(number);
+	});
+
+	it("Should set to 9 an empty cell", () => {
+		const cell = new Cell();
+		const number = 9;
+		expect(cell.number).toBeNull();
+		expect(cell.put(number, CellType.Written)).toBeTrue();
+		expect(cell.number).toBe(number);
+	});
+
+	it("Should add numbers to drafts", () => {
+		const cell = new Cell();
+		expect(cell.put(1, CellType.Draft)).toBeTrue();
+		expect(cell.drafts).toContainValue(1);
+		expect(cell.drafts.length).toBe(1);
+		expect(cell.put(1, CellType.Draft)).toBeFalse();
+		expect(cell.drafts.length).toBe(1);
+		expect(cell.put(9, CellType.Draft)).toBeTrue();
+		expect(cell.drafts).toContain(9);
+		expect(cell.drafts).toContainValue(9);
+		expect(cell.drafts.length).toBe(2);
+	});
+
+	it("Should not add illegal numbers to drafts", () => {
+		const cell = new Cell();
+		expect(cell.put(0, CellType.Draft)).toBeFalse();
+		expect(cell.put(10, CellType.Draft)).toBeFalse();
+		expect(cell.put(Number.MIN_SAFE_INTEGER, CellType.Draft)).toBeFalse();
+		expect(cell.put(Number.MAX_SAFE_INTEGER, CellType.Draft)).toBeFalse();
+		expect(cell.drafts.length).toBe(0);
+	});
+
+	it("Should not set an off-range number", () => {
+		const cell = new Cell();
+		expect(cell.put(10, CellType.Written)).toBeFalse();
+		expect(cell.put(-1, CellType.Written)).toBeFalse();
+		expect(cell.put(Number.MIN_SAFE_INTEGER, CellType.Written)).toBeFalse();
+		expect(cell.put(Number.MAX_SAFE_INTEGER, CellType.Written)).toBeFalse();
+	});
+
+	it("Should set to final", () => {
+		const cell = new Cell();
+		expect(cell.finalize()).toBeFalse();
+		expect(cell.put(1, CellType.Written)).toBeTrue();
+		expect(cell.finalize()).toBeTrue();
+		expect(cell.type).toBe(CellType.Final);
+		expect(cell.put(1, CellType.Written)).toBeFalse();
+	});
 });
 
 describe("grid tests", () => {});
