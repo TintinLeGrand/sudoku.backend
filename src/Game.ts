5bc3fda73b3d2c4f4eead126601465a165381df5
TintinLeGrand <ethan.leneindre9@gmail.com>
2026-03-17 17:17:43 +0100

    feat: Add empty Game class

--- src/Game.ts
+++ src/Game.ts
@@ -1,1 +1,15 @@
-export class Game {}
+import { Grid } from "./Grid";
+
+export class Game {
+	time: number;
+	private _grid: Grid;
+	private _paused: boolean;
+
+	constructor(gridSize: number = 9) {
+		this.time = 0;
+		this._grid = new Grid(gridSize);
+		this._paused = true;
+	}
+
+	public start() {}
+}
