e935964897347f1b3f310339ea1ac097901139a6
TintinLeGrand <ethan.leneindre9@gmail.com>
2026-03-18 15:15:07 +0100

    feat: Algorithm first part

---
+++ algorithm/grid.ts
@@ -0,0 +1,68 @@
+import { Cell, CellCategory } from "../src/Cell";
+import { Grid } from "../src/Grid";
+
+export enum Difficulty {
+	Easy = "easy",
+	Medium = "medium",
+	Hard = "hard",
+	Expert = "expert",
+}
+
+export interface Sudoku {
+	difficulty: Difficulty;
+	puzzle: Grid;
+	solution: Grid;
+}
+
+const SUPPRESSION_RATIO: Record<Difficulty, number> = {
+	[Difficulty.Easy]: 0.5,
+	[Difficulty.Medium]: 0.6,
+	[Difficulty.Hard]: 0.65,
+	[Difficulty.Expert]: 0.73,
+};
+
+const STRATEGIES = ["line", "column", "block"];
+
+function randomCells(size: number): Cell[] {
+	var numbers: number[] = [];
+	var cells: Cell[] = [];
+	for (let i = 0; i < size; i++) {
+		cells[i] = new Cell();
+		numbers.push(i + 1);
+	}
+
+	for (let i = 0; i < size; i++) {
+		const choice = Math.floor(Math.random() * (numbers.length - 1 + 1));
+		cells[i]!.put(numbers[choice]!, CellCategory.Normal);
+
+		numbers.splice(choice, 1);
+	}
+
+	return cells;
+}
+
+function generateGrid(grid: Grid, difficulty: Difficulty) {
+	var sudoku: Sudoku;
+
+	const pos = Math.floor(Math.random() * (grid.size - 1 + 1));
+	const strategy = STRATEGIES[Math.floor(Math.random() * (2 + 1))];
+	const shuffledCells: Cell[] = randomCells(grid.size);
+
+	switch (strategy) {
+		case "line":
+			grid.putLine(pos, shuffledCells);
+			break;
+		case "column":
+			grid.putColumn(pos, shuffledCells);
+			break;
+		case "block":
+			grid.putBlock(pos, shuffledCells);
+			break;
+		default:
+			console.error("Strategy choice does not exist");
+	}
+
+	grid.displayGrid();
+}
+
+for (let i = 0; i < 10; i++) generateGrid(new Grid(), Difficulty.Medium);
--- index.ts
+++ index.ts
@@ -1,1 +1,2 @@
+export * from "./algorithm/grid";
 export * from "./src/index";
